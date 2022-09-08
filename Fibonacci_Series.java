940791ea81c7ad2099ac317965710e3454f6f749
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-09-08 15:54:08 +0530

    Coupon Number

---
+++ Fibonacci_Series.java
@@ -0,0 +1,27 @@
+package com.bl.Day6;
+
+import java.util.Scanner;
+
+public class Fibonacci_Series {public static void checkSeries() {
+	int firstTerm=0;
+	int secondTerm=1;
+
+	Scanner sc = new Scanner(System.in);  
+
+	System.out.println("Enter the value of A :");
+	int number= sc.nextInt();
+
+	for (int i=0; i <= number; ++i ) {
+		int	nextTerm = firstTerm + secondTerm ;
+		firstTerm = secondTerm;
+		secondTerm = nextTerm ;
+
+		System.out.println("The value of fibanocci series is :" + nextTerm);
+	}
+	sc.close();
+}
+public static void main(String[] args) {
+	checkSeries();
+}
+
+}
